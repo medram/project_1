1a0f60abf60ccbe10f1503efb84db50b0d2822a5
medram <mramouchy@gmail.com>
2021-05-23 01:32:51 +0100

    updated migrations: added countries & publisher_rates tables

--- application/migrations/Version20210520000123.php
+++ application/migrations/Version20210520000123.php
@@ -14,18 +14,82 @@ final class Version20210520000123 extends AbstractMigration
 {
     public function getDescription(): string
     {
+        $this->site_name      = trim(_addslashes(strip_tags(getenv('SITE_NAME'))));
+        $this->support_email  = trim(strtolower(_addslashes(strip_tags(getenv('SITE_CONTACT_US_EMAIL')))));
+        $this->sub_folder     = get_sub_folder();
+        $this->time           = time();
+
+        $this->admin_name     = trim(_addslashes(strip_tags(getenv('USER_USERNAME'))));
+        $this->admin_email    = trim(strtolower(_addslashes(strip_tags(getenv('USER_EMAIL')))));
+        $this->admin_pass     = trim(_addslashes(strip_tags(getenv('USER_PASSWORD'))));
+
+        $this->db_prefix      = trim(strtolower(_addslashes(strip_tags(getenv('DB_PREFIX')))));
+        $this->db_hostname    = trim(_addslashes(strip_tags(getenv('DB_HOSTNAME'))));
+        $this->db_name        = trim(_addslashes(strip_tags(getenv('DB_NAME'))));
+        $this->db_username    = trim(_addslashes(strip_tags(getenv('DB_USER'))));
+        $this->db_password    = trim(_addslashes(strip_tags(getenv('DB_PASSWORD'))));
+
         return '';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        // Countries table
+        $countries = $schema->createTable($this->db_prefix.'countries');
+        $countries->addColumn('id', 'integer', [
+            'autoincrement' => true
+        ]);
+        $countries->addColumn('name', 'string', [
+            'notnull' => true
+        ]);
+        $countries->addColumn('code', 'string', [
+            'notnull' => true
+        ]);
 
+        $countries->setPrimaryKey(['id']);
+
+        // Publishers table
+        $pub_table = $schema->createTable($this->db_prefix.'publisher_rates');
+        $pub_table->addColumn('id', 'integer', [
+            'autoincrement' => true,
+        ]);
+        $pub_table->addColumn('country_id', 'integer');
+        $pub_table->addColumn('price', 'float', [
+            'default' => 0
+        ]);
+        $pub_table->addColumn('active', 'boolean', ['default' => true]);
+
+        $pub_table->setPrimaryKey(['id']);
+        $pub_table->addForeignKeyConstraint($countries, ['country_id'], ['id'], [
+            'onUpdate' => 'CASCADE'
+        ]);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $schema->dropTable($this->db_prefix.'publisher_rates');
+        $schema->dropTable($this->db_prefix.'countries');
+    }
 
+    public function postUp(Schema $schema): void
+    {
+        // insert some default countries
+        $this->write('Inserting countries...');
+        $i = 1;
+        foreach(get_default_countries() as $country)
+        {
+            // inserting countries.
+            $this->connection->insert($this->db_prefix.'countries', [
+                'name' => $country['name'],
+                'code' => $country['code']
+            ]);
+
+            // setting default values.
+            $this->connection->insert($this->db_prefix.'publisher_rates', [
+                'country_id' => $i,
+                'price' => 0.4
+            ]);
+            $i++;
+        }
     }
 }
